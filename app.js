20fde57a88b8a0b7366ccd19821ba5a7ba64998b
Vivek Chandra <vchandrajoshi@twitter.com>
2020-08-20 01:00:31 +0530

    email function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,7 +13,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/send-message", (req, res) => {
-  res.status(200).send("test data");
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "vjoshi139@gmail.com",
+      pass: "9058718893",
+    },
+  });
+
+  var mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "vjoshi139@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>",
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+
+  res.status(200).send("test");
   // We will be coding here
 });
 
